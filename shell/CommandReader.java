3ccb789c73299a10d41fd9b369980b6dbaff6b21
catroot0 <drowning.dev1@gmail.com>
2025-08-05 00:04:32 +0430

    fixing the bug that would print Command '' does not exist! when you just hit enter without entering any command

--- shell/CommandReader.java
+++ shell/CommandReader.java
@@ -1,5 +1,6 @@
 package shell;
 
+import java.beans.Expression;
 import java.util.Scanner;
 
 public class CommandReader {
@@ -9,12 +10,13 @@ public class CommandReader {
     while (true) {
       System.out.print("> ");
       String command = scanner.nextLine();
+      if (command.trim().isEmpty()) continue;
 
       boolean isValidCommand = CommandHandler.IsValidCommand(command);
       if (!isValidCommand) CommandHandler.SendError(command);
 
       boolean isExit = CommandHandler.HandleExit(command);
-      
+
       if (isExit) break;
     }
   }
